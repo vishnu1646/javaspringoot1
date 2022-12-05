7bd08654da0fd0005595fc9029fad6f7a47a01fc
vishnu1646 <vishnulance98@gmail.com>
2022-12-05 14:33:50 +0530

    springboot

---
+++ src/main/java/com/example/studentapp_backend3/StudentappBackend3Application.java
@@ -0,0 +1,14 @@
+package com.example.studentapp_backend3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentappBackend3Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StudentappBackend3Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/studentapp_backend3/controller/StudentController.java
@@ -0,0 +1,14 @@
+package com.example.studentapp_backend3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class StudentController {
+
+    @GetMapping("/")
+    public String Homepage(){
+        return "Welcome to my website";
+    }
+}
